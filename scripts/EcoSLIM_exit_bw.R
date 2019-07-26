4d8d01d8f7a5b110819be5b5e24c51f3b66a92b9
Garrett Rapp <grapp@email.arizona.edu>
2019-07-25 20:23:52 -0700

    updated script

--- scripts/EcoSLIM_exit_bw.R
+++ scripts/EcoSLIM_exit_bw.R
@@ -1,8 +1,8 @@
 # EcoSLIM output analysis script - 20190520 grapp
 # adapted from Reed_EcoSLIM_script
 # read binary particle file
-exit_file <- "~/Desktop/test/A_v1_EcoSLIM/bw_20190723/0100ppc/SLIM_A_v1_bw_pulse_100ppc_exited_particles.bin"
-restart_file <- "~/Desktop/test/A_v1_EcoSLIM/SLIM_A_v1_bw_ppul2_1000_particle_restart.bin"
+exit_file <- "~/Downloads/SLIM_A_v1_bw_pulse_1000ppc_exited_particles.bin"
+restart_file <- "~/Downloads/SLIM_A_v1_bw_pulse_1000ppc_particle_restart.bin"
 
 library(ggplot2)
 library(tidyr)
@@ -29,18 +29,23 @@ exited_particles$age_hr <- exited_particles$age
 exited_particles$age <- exited_particles$age_hr/24
 
 
-ggplot(restart_particles, aes(x=X, y=Y)) + geom_point()
+ggplot(exited_particles, aes(x=X, y=Z)) + geom_point() + scale_x_continuous(limits = c(0,8190), expand=c(0,0)) +
+  scale_y_continuous(limits = c(0,1000), expand=c(0,0))
+
+ggplot(restart_particles, aes(x=X, y=sat_age)) + geom_point()
+
+
 
 paste("Maximum particle age is", format(max(exited_particles$age), nsmall = 1), "days")
 
-pdf_exited_all <- pdfxn(exited_particles, max(exited_particles$age), 365)
+pdf_exited_all <- pdfxn(exited_particles, max(exited_particles$age), 1000)
 max_density <- max(pdf_exited_all$Density)
 pdf_exited_all$Density_norm <- pdf_exited_all$Density/max_density
 
 pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm), color="red") +
   #geom_line(data = pdf_exited_all_100, aes(x = age,y = Density_norm), color="blue") +
   #geom_line(data = pdf_exited_all_500, aes(x = age,y = Density_norm), color="green") +
-  geom_line(data = pdf_exited_all_1000, aes(x = age,y = Density_norm), color="orange") +
+  #geom_line(data = pdf_exited_all_1000, aes(x = age,y = Density_norm), color="orange") +
   scale_x_log10(name="Age (days)",limits = c(100,500000), breaks = scales::trans_breaks("log10", function(x) 10^x), 
     labels = scales::trans_format("log10", scales::math_format(10^.x)), expand=c(0,0)) + annotation_logticks(base =10, sides = "b") +
   ggtitle("PDF of all exited particles for Scenario A (backwards tracking)") + 
@@ -49,7 +54,7 @@ pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_
 pdf_fig1
 
 hist_fig <- ggplot(exited_particles, aes(age)) + geom_histogram(binwidth = 365, color = "red", fill = "red") + ggtitle("Histogram of all particles exiting the domain for Scenario A") + 
-  scale_y_continuous(name="Particle Count",labels = scales::comma, expand=c(0,0),breaks = seq(0,250,50), limits = c(0,250)) + 
+  scale_y_continuous(name="Particle Count",labels = scales::comma, expand=c(0,0),breaks = seq(0,10,2), limits = c(0,10)) + 
   scale_x_continuous(name="Age (days)", expand=c(0,0),breaks = seq(0,85000,10000), limits = c(0,85000),labels = scales::comma) +
   expand_limits(x = 0, y = 0)
 hist_fig
