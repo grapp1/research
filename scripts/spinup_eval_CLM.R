cc5abdeb87374c3f1d52e6ee6aebb3904568cb92
Garrett Rapp <grapp@email.arizona.edu>
2019-08-22 13:35:46 -0700

    wrote more of CLM spinup eval script

--- scripts/spinup_eval_CLM.R
+++ scripts/spinup_eval_CLM.R
@@ -1,3 +1,25 @@
 # 20190815 spinup_eval_CLM
 # checking spinup status of CLM runs
 
+# step 1 - must run Calc_Water_Balance.tcl
+require(ggplot2)
+
+
+water_budget.df <- read.table("/Users/grapp/Desktop/test/A_v2_outputs_all/wb_A_v2.txt", header = TRUE,sep = "\t")
+water_budget.df <- water_budget.df[-c(6)]
+water_budget.df$avg_52wk <- 0
+water_budget.df$avg_5yr <- 0
+water_budget.df$avg_5yr_pct <- 0
+for(i in 52:nrow(water_budget.df)){
+  water_budget.df$avg_52wk[i] <- sum(water_budget.df$Total_subsurface_storage[(i-51):i])/52
+}
+for(i in 260:nrow(water_budget.df)){
+  water_budget.df$avg_5yr[i] <- sum(water_budget.df$Total_subsurface_storage[(i-259):i])/260
+}
+for(i in 261:nrow(water_budget.df)){
+  water_budget.df$avg_5yr_pct[i] <- (water_budget.df$avg_5yr[i]-water_budget.df$avg_5yr[i-1])/water_budget.df$avg_5yr[i]
+}
+
+storage_fig <- ggplot() + geom_line(data = water_budget.df[261:nrow(water_budget.df),], aes(Hour,avg_5yr_pct))
+storage_fig
+
