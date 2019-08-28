28610595c8029b04a4d2353f2fd3576248cb12d6
Garrett Rapp <grapp@email.arizona.edu>
2019-08-28 10:41:51 -0700

    wrote script to generate pulse points for EcoSLIM runs

---
+++ scripts/EcoSLIM_pulse_gen.R
@@ -0,0 +1,22 @@
+# 20190509 creation of geology indicator file for domain in simple ascii format
+
+# setting grid and initializing matrix for output
+nx <- 91
+ny <- 70
+nz <- 20
+
+pulse_start <- matrix(0, nrow = nx*ny*nz)
+
+
+# layer numbers for indicator file (from bottom to top)
+x_cell <- 50
+y_cell <- 12
+
+rownum <- (nx*ny*(nz-1)) + x_cell + (nx*(y_cell-1))
+pulse_start[rownum] <- -3.500000e-04
+
+pulse_start = rbind(c("91 70 20"), pulse_start)
+
+write.table(pulse_start, file = "/Users/grapp/Desktop/working/A_v3/pulse_files/A_v3_bw3.sa",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
