9e2eec823d0d09a22ffb88286f8de1aa68046b84
Garrett Rapp <grapp@email.arizona.edu>
2019-09-09 09:57:57 -0700

    exported A_v3 figures

--- scripts/EcoSLIM_exit_bw.R
+++ scripts/EcoSLIM_exit_bw.R
@@ -36,6 +36,8 @@ paste("Maximum particle age is", sprintf("%02g",max(exited_particles_2$age)/(24*
 exited_particles <- rbind(exited_particles_1,exited_particles_2)
 exited_particles <- exited_particles[!duplicated(exited_particles),]
 
+exited_particles <- exited_particles_1
+
 # converting age to years, but still keeping the hours column
 exited_particles$age_hr <- exited_particles$age  
 exited_particles$age <- exited_particles$age_hr/(24*365)
@@ -50,28 +52,34 @@ pdf_exited_all <- pdfxn(exited_particles, max(exited_particles$age), 3)
 paste("Maximum particle age is", sprintf("%02g",max(exited_particles$age)), "years")
 
 # updated exit_pts chart - need to run surf_flow_domain.R before this to generate dem_fig
-exit_pts <- flowpath_fig + geom_point(data = restart_particles_1, aes(x=X, y=Y, colour = age)) + labs(color = "Age (years)") +
-  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(100,800),breaks=c(seq(0,600,100)), 
-                        labels=c("0","≤100","200","300","400","500","600")) +
-  ggtitle("Locations and ages of exited particles for A_v3 - backwards tracking from cell [38,32]")
+exit_pts <- flowpath_fig + geom_point(data = exited_particles, aes(x=X, y=Y, colour = age)) + labs(color = "Age (years)") +
+  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(100,800),breaks=c(seq(0,800,100)), 
+                        labels=c("0","≤100","200","300","400","500","600","700","800")) +
+  ggtitle("Locations and ages of exited particles for A_v3 - backwards tracking from cell [38,17]")
 
 exit_pts
 
-#pdf_exit_bw1 <- pdf_exited_all
-#save(pdf_exit_bw1,file="~/research/Scenario_A/A_v3/pdf_exit_bw1.Rda")
+pdf_exit_bw4 <- pdf_exited_all
 
+exited_particles_bw1 <- exited_particles
+save(exited_particles_bw1,file="~/research/Scenario_A/A_v3/exited_particles_bw1.Rda")
+
+load(file="~/research/Scenario_A/A_v3/pdf_exit_bw1.Rda")
 load(file="~/research/Scenario_A/A_v3/pdf_exit_bw2.Rda")
+load(file="~/research/Scenario_A/A_v3/pdf_exit_bw4.Rda")
+pdf_exit_bw1$st_cell <- "[4,22]"
+pdf_exit_bw2$st_cell <- "[15,32]"
+pdf_exit_bw4$st_cell <- "[38,17]"
+pdf_exited_all <- rbind(pdf_exit_bw1,pdf_exit_bw2,pdf_exit_bw4)
 
-pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm), color="red") +
-  geom_line(data = pdf_exit_bw1, aes(x = age,y = Density_norm), color="black") +
-  geom_line(data = pdf_exit_bw2, aes(x = age,y = Density_norm), color="blue") +
+pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm, group=st_cell,col = st_cell)) +
   #geom_line(data = pdf_exit_5k, aes(x = age,y = Density_norm), color="green") +
   #geom_line(data = pdf_exit_10k, aes(x = age,y = Density_norm), color="orange") +
   scale_x_log10(name="Age (years)",limits = c(100,1000), breaks = scales::trans_breaks("log10", function(x) 10^x), 
     labels = scales::trans_format("log10", scales::math_format(10^.x)), expand=c(0,0)) + annotation_logticks(base =10, sides = "b") +
   ggtitle("PDF of all exited particles for Scenario A (backward tracking at different locations)") + 
   scale_y_continuous(name="Normalized Density", expand=c(0,0), breaks = seq(0,1,0.1), limits = c(0,1)) + 
-  scale_color_manual(values = c("firebrick", "dodgerblue","green"))  +
+  scale_color_manual(values = c("firebrick", "dodgerblue","black"))  + labs(color = "Starting Cell") +
   expand_limits(x = 100, y = 0) + theme_bw() + 
   theme(panel.border = element_rect(colour = "black", size=1, fill=NA), panel.grid.major = element_line(colour="grey", size=0.1), legend.position="right")
 pdf_fig1
