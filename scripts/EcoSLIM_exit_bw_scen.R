1b4f45c89b3690a79450ed440010243de7dc88a8
Garrett Rapp <grapp@email.arizona.edu>
2019-09-15 16:19:55 -0400

    updates

--- scripts/EcoSLIM_exit_bw_scen.R
+++ scripts/EcoSLIM_exit_bw_scen.R
@@ -20,7 +20,7 @@ restart_file_1 <- "/Users/grapp/Desktop/working/A_v3/pulse_files/SLIM_A_v3_bw6_p
 restart_particles_1 <- ES_read(restart_file_1, type = "restart")
 
 
-exit_file_A <- "/Users/grapp/Downloads/SLIM_A_v3_bw5_exited_particles.bin"
+exit_file_A <- "/Users/garrettrapp/Desktop/ecoslim_B_bw5/SLIM_A_v3_bw5_exited_particles.bin"
 exited_particles_A <- ES_read(exit_file_A, type = "exited")
 paste("Maximum particle age is", sprintf("%02g",max(exited_particles_A$age)/(24*365)), "years")
 exit_file_A2 <- "/Users/grapp/Desktop/working/A_v3/bw_20190903/20190908_dl/SLIM_A_v3_bw4_exited_particles.bin"
@@ -30,16 +30,16 @@ exited_particles_A <- rbind(exited_particles_A1,exited_particles_A2)
 exited_particles_A <- exited_particles_A[!duplicated(exited_particles_A),]
 
 #exit_file_B1 <- "/Users/grapp/Desktop/working/B_v2_ES_local/first_run/SLIM_B_v2_bw3_exited_particles.bin"
-exit_file_B <- "/Users/grapp/Desktop/working/B_v2/bw_20190911/bw4/SLIM_B_v2_bw4_exited_particles.bin"
-exited_particles_B <- ES_read(exit_file_B, type = "exited")
-paste("Maximum particle age is", sprintf("%02g",max(exited_particles_B$age)/(24*365)), "years")
-exit_file_B2 <- "/Users/grapp/Desktop/working/B_v2_ES_local/bw3/SLIM_B_v2_bw3_exited_particles.bin"
+exit_file_B1 <- "/Users/garrettrapp/Desktop/ecoslim_B_bw5/SLIM_B_v2_bw5_exited_particles_1.bin"
+exited_particles_B1 <- ES_read(exit_file_B1, type = "exited")
+paste("Maximum particle age is", sprintf("%02g",max(exited_particles_B1$age)/(24*365)), "years")
+exit_file_B2 <- "/Users/garrettrapp/Desktop/ecoslim_B_bw5/SLIM_B_v2_bw5_exited_particles_2.bin"
 exited_particles_B2 <- ES_read(exit_file_B2, type = "exited")
 paste("Maximum particle age is", sprintf("%02g",max(exited_particles_B2$age)/(24*365)), "years")
 exited_particles_B <- rbind(exited_particles_B1,exited_particles_B2)
 exited_particles_B <- exited_particles_B[!duplicated(exited_particles_B),]
 
-exit_file_C <- "/Users/grapp/Desktop/working/C_v2_outputs/bw_20190911/bw4/SLIM_C_v2_bw4_exited_particles.bin"
+exit_file_C <- "/Users/garrettrapp/Desktop/ecoslim_B_bw5/SLIM_C_v2_bw4_exited_particles.bin"
 exited_particles_C <- ES_read(exit_file_C, type = "exited")
 paste("Maximum particle age is", sprintf("%02g",max(exited_particles_C$age)/(24*365)), "years")
 
@@ -56,14 +56,14 @@ exited_particles_C <- exited_particles_C[exited_particles_C$age > 1,]
 
 # generating pdf
 pdf_exit_A_bw5 <- pdfxn(exited_particles_A, max(exited_particles_A$age), 1)
-pdf_exit_B_bw4 <- pdfxn(exited_particles_B, max(exited_particles_B$age), 1)
+pdf_exit_B_bw5 <- pdfxn(exited_particles_B, max(exited_particles_B$age), 1)
 pdf_exit_C_bw4 <- pdfxn(exited_particles_C, max(exited_particles_C$age), 1)
 
 # updated exit_pts chart - need to run surf_flow_domain.R before this to generate dem_fig
 exit_pts <- flowpath_fig + geom_point(data = exited_particles_B, aes(x=X, y=Y, colour = age)) + labs(color = "Age (years)") +
-  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(5,200),breaks=c(5,10,20,50,100,200), 
-                        labels=c("≤5","10","20","50","100","200")) +
-  ggtitle("Locations and ages of exited particles for Scenario B - backwards tracking from cell [20,22]")
+  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(50,400),breaks=c(50,100,200,300,400), 
+                        labels=c("≤50","100","200","300","400")) +
+  ggtitle("Locations and ages of exited particles for Scenario B - backwards tracking from cell [12,19]")
 
 exit_pts
 
@@ -81,12 +81,13 @@ pdf_exit_bw4$st_cell <- "[38,17]"
 pdf_exited_all <- rbind(pdf_exit_bw1,pdf_exit_bw2,pdf_exit_bw4)
 
 pdf_exit_A_bw5$scen <- "A"
-pdf_exit_B_bw4$scen <- "B"
+pdf_exit_B_bw5$scen <- "B"
 pdf_exit_C_bw4$scen <- "C"
-pdf_exited_all <- rbind(pdf_exit_A_bw5,pdf_exit_B_bw4,pdf_exit_C_bw4)
+pdf_exited_all <- rbind(pdf_exit_A_bw5,pdf_exit_B_bw5,pdf_exit_C_bw4)
 
-pdf_exit_c3817 <- pdf_exited_all
-save(pdf_exit_c3817,file="~/research/EcoSLIM/pdf_exit_c3817_ABC.Rda")
+pdf_exit_c2022 <- pdf_exited_all
+save(pdf_exit_c2022,file="~/research/EcoSLIM/pdf_exit_c2022_ABC.Rda")
+load(file="~/research/EcoSLIM/pdf_exit_c3817_ABC.Rda")
 
 mult <- 100
 
@@ -94,8 +95,8 @@ pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_
 #pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm)) +
   #scale_x_log10(name="Age (years)",limits = c(100,1000), breaks = scales::trans_breaks("log10", function(x) 10^x), 
   #  labels = scales::trans_format("log10", scales::math_format(10^.x)), expand=c(0,0)) + annotation_logticks(base =10, sides = "b") +
-  scale_x_log10(name="Age (years)",limits = c(1,400), breaks = c(10,50,100,200,300,400),labels = scales::comma,expand=c(0,0)) +
-  ggtitle("PDF of all exited particles - backward tracking from cell [38,17]") + 
+  scale_x_log10(name="Age (years)",limits = c(2,400), breaks = c(2,10,50,100,200),labels = scales::comma,expand=c(0,0)) +
+  ggtitle("PDF of all exited particles - backward tracking from cell [20,22]") + 
   scale_y_continuous(name=expression(Density~("x10"^-2)), expand=c(0,0), breaks = seq(0,0.1,0.01)*mult, limits = c(0,0.1)*mult) + 
   scale_color_manual(values = c("firebrick", "dodgerblue","green"))  + labs(color = "Scenario") +
   expand_limits(x = 100, y = 0) + theme_bw() + 
