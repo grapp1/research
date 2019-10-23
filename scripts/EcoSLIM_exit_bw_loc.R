311713ac94642084f9aeda3b5dee0f5c26db7435
Garrett Rapp <grapp@email.arizona.edu>
2019-10-23 16:00:56 -0700

    udpated scripts to calculate pdfs for path length and saturated path length

--- scripts/EcoSLIM_exit_bw_loc.R
+++ scripts/EcoSLIM_exit_bw_loc.R
@@ -74,20 +74,29 @@ exited_particles_A_bw3$age <- exited_particles_A_bw3$age_hr/(24*365)
 exited_particles_A_bw3 <- exited_particles_A_bw3[exited_particles_A_bw3$age > 1,] 
 
 # updated exit_pts chart - need to run surf_flow_domain.R before this to generate dem_fig
-exit_pts <- flowpath_fig + geom_point(data = exited_particles_A_bw2, aes(x=X, y=Y, colour = age)) + labs(color = "Age (years)") +
-  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(50,600),breaks=c(50,100,200,300,400,500,600), 
+exit_pts <- flowpath_fig + geom_point(data = exited_particles_A_bw1, aes(x=X, y=Y, colour = age)) + labs(color = "Age (years)") +
+  scale_colour_gradient(low = "white", high="midnightblue", trans = "log",limits=c(50,700),breaks=c(50,100,200,300,400,500,600), 
                         labels=c("≤50","100","200","300","400","500","600")) +
   #guides(color = guide_legend(override.aes = list(size = 5))) +
-  ggtitle("Locations and ages of exited particles for Scenario A - backwards tracking from cell [38,17]")
+  ggtitle("Locations and ages of exited particles for Scenario A - backwards tracking from cell [4,22]")
 
 exit_pts
 
 
 # generating pdf
-bin_size <- 2
-pdf_exit_A_bw1 <- pdfxn(exited_particles_A_bw1, max(exited_particles_A_bw1$age), bin_size)
-pdf_exit_A_bw2 <- pdfxn(exited_particles_A_bw2, max(exited_particles_A_bw2$age), bin_size)
-pdf_exit_A_bw3 <- pdfxn(exited_particles_A_bw3, max(exited_particles_A_bw3$age), bin_size)
+bin_size_age <- 2
+pdf_exit_A_bw1 <- pdfxn(exited_particles_A_bw1, max(exited_particles_A_bw1$age), bin_size_age, column = "age")
+pdf_exit_A_bw2 <- pdfxn(exited_particles_A_bw2, max(exited_particles_A_bw2$age), bin_size_age, column = "age")
+pdf_exit_A_bw3 <- pdfxn(exited_particles_A_bw3, max(exited_particles_A_bw3$age), bin_size_age, column = "age")
+
+bin_size_path <- 200
+pdf_exit_A_bw1_path <- pdfxn(exited_particles_A_bw1, max(exited_particles_A_bw1$path_len), bin_size_path, column = "path_len")
+pdf_exit_A_bw2_path <- pdfxn(exited_particles_A_bw2, max(exited_particles_A_bw2$path_len), bin_size_path, column = "path_len")
+pdf_exit_A_bw3_path <- pdfxn(exited_particles_A_bw3, max(exited_particles_A_bw3$path_len), bin_size_path, column = "path_len")
+
+pdf_exit_A_bw1_spath <- pdfxn(exited_particles_A_bw1, max(exited_particles_A_bw1$path_len), bin_size_path, column = "spath_len")
+pdf_exit_A_bw2_spath <- pdfxn(exited_particles_A_bw2, max(exited_particles_A_bw2$path_len), bin_size_path, column = "spath_len")
+pdf_exit_A_bw3_spath <- pdfxn(exited_particles_A_bw3, max(exited_particles_A_bw3$path_len), bin_size_path, column = "spath_len")
 
 
 ### pdf plotting
@@ -108,6 +117,40 @@ pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_
   expand_limits(x = 100, y = 0) + theme_bw() + 
   theme(panel.border = element_rect(colour = "black", size=1, fill=NA), panel.grid.major = element_line(colour="grey", size=0.1), legend.position="right")
 pdf_fig1
+
+pdf_exit_A_bw1_path$pt <- "[4,22]"
+pdf_exit_A_bw2_path$pt <- "[38,17]"
+pdf_exit_A_bw3_path$pt <- "[10,28]"
+pdf_exited_all_path <- rbind(pdf_exit_A_bw1_path,pdf_exit_A_bw2_path,pdf_exit_A_bw3_path)
+
+pdf_fig2 <- ggplot() + geom_line(data = pdf_exited_all_path, aes(x = path_len,y = Density_pdf, group=pt,col = pt)) +
+  #pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm)) +
+  #scale_x_log10(name="Age (years)",limits = c(100,1000), breaks = scales::trans_breaks("log10", function(x) 10^x), 
+  #  labels = scales::trans_format("log10", scales::math_format(10^.x)), expand=c(0,0)) + annotation_logticks(base =10, sides = "b") +
+  scale_x_log10(name="Particle Path Length (m)",limits = c(10000,60000), breaks = c(10000,20000,40000,50000,60000),labels = scales::comma,expand=c(0,0)) +
+  ggtitle("PDF of path lengths for all exited particles - backward tracking for Scenario A") + 
+  scale_y_continuous(name="Density", expand=c(0,0), breaks = seq(0,0.1,0.01), limits = c(0,0.05)) + 
+  scale_color_manual(values = c("firebrick", "dodgerblue","darkgreen"))  + labs(color = "Starting Cell") +
+  expand_limits(x = 100, y = 0) + theme_bw() + 
+  theme(panel.border = element_rect(colour = "black", size=1, fill=NA), panel.grid.major = element_line(colour="grey", size=0.1), legend.position="right")
+pdf_fig2
+
+pdf_exit_A_bw1_spath$pt <- "[4,22]"
+pdf_exit_A_bw2_spath$pt <- "[38,17]"
+pdf_exit_A_bw3_spath$pt <- "[10,28]"
+pdf_exited_all_spath <- rbind(pdf_exit_A_bw1_spath,pdf_exit_A_bw2_spath,pdf_exit_A_bw3_spath)
+
+pdf_fig3 <- ggplot() + geom_line(data = pdf_exited_all_spath, aes(x = spath_len,y = Density_pdf, group=pt,col = pt)) +
+  #pdf_fig1 <- ggplot() + geom_line(data = pdf_exited_all, aes(x = age,y = Density_norm)) +
+  #scale_x_log10(name="Age (years)",limits = c(100,1000), breaks = scales::trans_breaks("log10", function(x) 10^x), 
+  #  labels = scales::trans_format("log10", scales::math_format(10^.x)), expand=c(0,0)) + annotation_logticks(base =10, sides = "b") +
+  scale_x_log10(name="Particle Saturated Zone Path Length (m)",limits = c(10000,60000), breaks = c(10000,20000,40000,50000,60000),labels = scales::comma,expand=c(0,0)) +
+  ggtitle("PDF of saturated path lengths for all exited particles - backward tracking for Scenario A") + 
+  scale_y_continuous(name="Density", expand=c(0,0), breaks = seq(0,0.1,0.01), limits = c(0,0.05)) + 
+  scale_color_manual(values = c("firebrick", "dodgerblue","darkgreen"))  + labs(color = "Starting Cell") +
+  expand_limits(x = 100, y = 0) + theme_bw() + 
+  theme(panel.border = element_rect(colour = "black", size=1, fill=NA), panel.grid.major = element_line(colour="grey", size=0.1), legend.position="right")
+pdf_fig3
 
 hist_fig <- ggplot(exited_particles, aes(age)) + geom_histogram(binwidth = 1000, color = "red", fill = "red") + ggtitle("Histogram of all particles exiting the domain for Scenario A (forward tracking)") + 
   scale_y_continuous(name="Particle Count",labels = scales::comma, expand=c(0,0),breaks = seq(0,30,2), limits = c(0,30)) + 
--- scripts/prob_dens_fxn_age.R
+++ scripts/prob_dens_fxn_age.R
@@ -1,18 +1,17 @@
 # 20190625 probability density function
 
 
-pdfxn <- function(df_exited,max_time,bin_length,column = "age"){
+pdfxn_age <- function(df_exited,max_time,bin_length){
   library(ggplot2)
 
   time_bins <- matrix(seq(0,max_time,bin_length))
   df_exited_pdf <- df_exited
-  col_num <- which(colnames(df_exited_pdf)==column)
-  df_exited_pdf$bin <- cut(df_exited_pdf[,col_num], c(time_bins), include.lowest = TRUE)
+  df_exited_pdf$bin <- cut(df_exited_pdf$age, c(time_bins), include.lowest = TRUE)
   
   df_exited_pdf <- aggregate(df_exited_pdf$mass, by = list(bin = df_exited_pdf$bin), FUN = sum)
   df_exited_pdf$age <- as.integer(df_exited_pdf$bin)*bin_length
   
-  colnames(df_exited_pdf) <- c("bin","Density",column)
+  colnames(df_exited_pdf) <- c("bin","Density","age")
   
   max_density <- max(df_exited_pdf$Density)
   df_exited_pdf$Density_norm <- df_exited_pdf$Density/max_density
