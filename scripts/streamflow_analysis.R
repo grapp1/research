90476398cf6d5ec292a7247fc3d48ab8c138708e
Garrett Rapp <grapp@email.arizona.edu>
2020-01-05 13:31:28 -0700

    updating streamflow script

--- scripts/streamflow_analysis.R
+++ scripts/streamflow_analysis.R
@@ -19,54 +19,79 @@ source("~/research/scripts/cell_agg_fxn.R")
 source("~/research/scripts/var_bin_fxn.R")
 source("~/research/scripts/particle_flowpath_fxn.R")
 
-precip <- read.table(file = "/Users/grapp/Desktop/working/CLM_data/Forcing1D_gr.txt", header = FALSE)
+precip <- read.table(file = "/Users/garrettrapp/research/CLM/Forcing1D_gr.txt", header = FALSE)
 precip <- data.frame(precip[1:8760,c(3)])
 colnames(precip) <- c("precip")
 precip$hour <- row(precip)
 plot(precip$precip)
 
 
-wbal_A <- read.table(file = "/Users/grapp/Downloads/wb_A_v6q2.txt", header = TRUE)
-
-
-wbal_C <- read.table(file = "/Users/grapp/Desktop/working/C_v4_outputs/wb_C_v4.txt", header = TRUE)
-wbal_D <- read.table(file = "/Users/grapp/Desktop/working/D_v4_outputs/wb_D_v4.txt", header = TRUE)
-wbal_E <- read.table(file = "/Users/grapp/Desktop/working/E_v1_outputs/wb_E_v1_pt2.txt", header = TRUE)
-wbal_F <- read.table(file = "/Users/grapp/Desktop/working/F_v1_working/wb_F_v1.txt", header = TRUE)
-wbal_A$scen <- "A"
-
+wbal_A <- read.table(file = "~/research/streamflow/wb_A_v6q2.txt", header = TRUE)
+wbal_B <- read.table(file = "~/research/streamflow/wb_B_v5q2.txt", header = TRUE)
+wbal_C <- read.table(file = "~/research/streamflow/wb_C_v5q.txt", header = TRUE)
+wbal_D <- read.table(file = "~/research/streamflow/wb_D_v5q.txt", header = TRUE)
+wbal_E <- read.table(file = "~/research/streamflow/wb_E_v2q.txt", header = TRUE)
+#wbal_F <- read.table(file = "~/research/streamflow/wb_F_v2q.txt", header = TRUE)
 wbal_A$day <- rep(1:365, each = 24)
 wbal_A <- aggregate(wbal_A$Total_surface_runoff, by = list(Category = wbal_A$day), FUN = sum)
 colnames(wbal_A) <- c("day", "Total_surface_runoff")
 wbal_A$Total_surface_runoff <- wbal_A$Total_surface_runoff/24
-
-
-
 wbal_A <- prob_exc(wbal_A)
 
-outflow_all <- rbind(wbal_A,wbal_A2)
+wbal_B$day <- rep(1:365, each = 24)
+wbal_B <- aggregate(wbal_B$Total_surface_runoff, by = list(Category = wbal_B$day), FUN = sum)
+colnames(wbal_B) <- c("day", "Total_surface_runoff")
+wbal_B$Total_surface_runoff <- wbal_B$Total_surface_runoff/24
+wbal_B <- prob_exc(wbal_B)
 
-outflow_all <- outflow_all[,c(1,4,6:8)]
-colnames(outflow_all) <- c("week","runoff_m3","scen","rank","prob_exc")
-outflow_all$week <- rep(1:521)
+wbal_C$day <- rep(1:365, each = 24)
+wbal_C <- aggregate(wbal_C$Total_surface_runoff, by = list(Category = wbal_C$day), FUN = sum)
+colnames(wbal_C) <- c("day", "Total_surface_runoff")
+wbal_C$Total_surface_runoff <- wbal_C$Total_surface_runoff/24
+wbal_C <- prob_exc(wbal_C)
 
-lm_qBC <- lm(wbal_B$Total_surface_runoff ~ wbal_C$Total_surface_runoff)
+wbal_D$day <- rep(1:365, each = 24)
+wbal_D <- aggregate(wbal_D$Total_surface_runoff, by = list(Category = wbal_D$day), FUN = sum)
+colnames(wbal_D) <- c("day", "Total_surface_runoff")
+wbal_D$Total_surface_runoff <- wbal_D$Total_surface_runoff/24
+wbal_D <- prob_exc(wbal_D)
+
+wbal_E$day <- rep(1:365, each = 24)
+wbal_E <- aggregate(wbal_E$Total_surface_runoff, by = list(Category = wbal_E$day), FUN = sum)
+colnames(wbal_E) <- c("day", "Total_surface_runoff")
+wbal_E$Total_surface_runoff <- wbal_E$Total_surface_runoff/24
+wbal_E <- prob_exc(wbal_E)
+
+wbal_A$scen <- "A"
+wbal_B$scen <- "B"
+wbal_C$scen <- "C"
+wbal_D$scen <- "D"
+wbal_E$scen <- "E"
+
+
+
+
+outflow_all <- rbind(wbal_A,wbal_B,wbal_C,wbal_D,wbal_E)
+
+#outflow_all <- outflow_all[,c(1,4,6:8)]
+colnames(outflow_all) <- c("day","runoff_m3","rank","prob_exc","scen")
+
+#lm_qBC <- lm(wbal_C$Total_surface_runoff ~ wbal_C$Total_surface_runoff)
 
 
 ## flow-duration curves
 ggplot() + geom_line(data = outflow_all, aes(x = prob_exc,y = runoff_m3, group=scen,col = scen), size = 1) +
-  geom_line(data = wbal_A3, aes(x = prob_exc,y = Total_surface_runoff), size = 1, color = "blue") +
   scale_color_manual(values = c("black","firebrick", "dodgerblue","darkorange","purple","green3"), guide = guide_legend(ncol = 3))  + labs(color = "Scenario") +
-  scale_y_log10(name="Discharge (m3/hr)",limits = c(400,120000), expand=c(0,0), breaks = c(500,1000,2000,5000,10000,30000,120000),labels = scales::comma) +
+  scale_y_log10(name="Discharge (m3/hr)",limits = c(400,10000), expand=c(0,0), breaks = c(500,1000,1300,2000,5000,10000),labels = scales::comma) +
   #scale_x_reverse(trans = "log10", name="Probability of exceedance",limits = c(1,0.001), expand=c(0,0), breaks = c(seq(0.001,0.01,0.1,1))) +
-  scale_x_log10(name="Probability of exceedance",limits = c(0.0002,1), expand=c(0,0), breaks = c(0.001,0.01,0.1,1.0)) +
+  scale_x_log10(name="Probability of exceedance",limits = c(0.002,1), expand=c(0,0), breaks = c(0.001,0.002,0.01,0.05,0.1,1.0)) +
   ggtitle(paste("Flow-duration curves")) + theme_bw() +
   theme(panel.border = element_rect(colour = "black", size=1, fill=NA), panel.grid.major = element_line(colour="grey", size=0.1), legend.position=c(0.8,0.8),
         legend.background = element_rect(linetype="solid", colour ="white"),plot.margin = margin(5,15,5,5),
         legend.text = element_text(color="black",size=12))
 
 ggplot() + geom_line(data = outflow_all, aes(x = week,y = runoff_m3, group=scen,col = scen), size = 1)
-ggplot() + geom_point(aes(x = wbal_B$Total_surface_runoff,y = wbal_C$Total_surface_runoff)) +
+ggplot() + geom_point(aes(x = wbal_C$Total_surface_runoff,y = wbal_C$Total_surface_runoff)) +
   #geom_abline(slope = 1, intercept = 0, col="purple",linetype = "twodash") +
   geom_abline(slope = 2, intercept = 1, col="red",linetype = "twodash") +
   scale_x_log10(name="Scenario A weekly surface runoff",limits = c(500,30000), expand=c(0,0), breaks = c(500,1000,10000,30000)) +
